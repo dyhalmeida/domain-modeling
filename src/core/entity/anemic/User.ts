6ee7db77c27b0e69796a919269389ffe2ef3c8c4
Diego Almeida <dyhalmeida@gmail.com>
2023-12-26 06:32:15 -0300

    feat: add anemic model with class
    Through testing, the fragility of anemic modeling is exposed. The object does not have any validation for it to be valid

--- src/core/entity/anemic/User.ts
+++ src/core/entity/anemic/User.ts
@@ -1,6 +1,8 @@
-export interface User {
-    id: number
-    name: string
-    email: string
-    password?: string
+export class User {
+    constructor(
+        public id: number,
+        public name: string,
+        public email: string,
+        public password?: string
+    ){}
 }
--- test/core/entity/anemic/User.test.ts
+++ test/core/entity/anemic/User.test.ts
@@ -2,30 +2,29 @@ import { User } from "@/core/entity/anemic/User";
 
 describe('Anemic User', () => {
 
-    const user: User = {
-        id: 999,
-        name: 'John Doe',
-        email: 'john.doe@mail.com',
-        password: '1234'
-    }
+    const builderUser = () => new User(999, 'John Doe', 'john.doe@mail.com', '1234')
 
     it('Should allow unnamed user', () => {
-        const unnamedUser: User = {...user, name: ''}
-        expect(unnamedUser.name).toBe('')
+        const user = builderUser()
+        user.name = ''
+        expect(user.name).toBe('')
     })
 
     it('Should allow user with negative id', () => {
-        const unnamedUser: User = {...user, id: -999}
-        expect(unnamedUser.id).toBe(-999)
+        const user = builderUser()
+        user.id = -999
+        expect(user.id).toBe(-999)
     })
 
     it('Should allow user with invalid email', () => {
-        const unnamedUser: User = {...user, email: '!@#$%'}
-        expect(unnamedUser.email).toBe('!@#$%')
+        const user = builderUser()
+        user.email = '!@#$%'
+        expect(user.email).toBe('!@#$%')
     })
 
     it('Should allow user with invalid password', () => {
-        const unnamedUser: User = {...user, password: 'a'}
-        expect(unnamedUser.password).toBe('a')
+        const user = builderUser()
+        user.password = 'a'
+        expect(user.password).toBe('a')
     })
 })
